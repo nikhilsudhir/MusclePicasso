1540873ec489376617992c2c842443c41093d700
Nikhil Sudhir <nikhilsudhir23@gmail.com>
2026-03-15 23:22:31 +1100

    updated name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/musclePicasso/',
+  base: '/MusclePicasso/',
   plugins: [react()],
   server: {
     port: 3000,
